ef8f4099eb623069a348d0959df1c49d9a823dbf
Black-Cog <blackcogdigital@gmail.com>
2015-03-04 07:05:51 +0100

    Added publishModelingPrint

--- core/HammerActions.py
+++ core/HammerActions.py
@@ -5,3 +5,25 @@ class HammerActions():
 	@staticmethod
 	def mail():
 		print 'email forwarded'
+
+	@staticmethod
+	def launchPublish( filename2=None, classname2=None ):
+		import sys
+		sys.path.append( 'F:/dev/Hammer/maya/actions/' )
+
+		filename  = 'FnPublishModelingPrint'
+		classname = 'Modeling'
+		importcmd   = 'from %s import %s' %( filename, classname )
+		instancecmd = 'inst = %s()' %( classname )
+
+		exec( importcmd )
+		exec( instancecmd )
+
+		instMethods = dir( inst )
+
+
+		if 'verification' in instMethods:
+			print 'verification'
+
+		if 'publish' in instMethods:
+			print 'publish'
