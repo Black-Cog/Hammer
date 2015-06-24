9293981bc46deb27274bc8e1639908f3f1c704fd
Black-Cog <blackcogdigital@gmail.com>
2015-06-24 09:12:19 +0200

    Fixed openScene Action.
    Fixed openScene Action.

--- core/Actions/BaseEntityOpenScene.py
+++ core/Actions/BaseEntityOpenScene.py
@@ -31,44 +31,50 @@ class BaseEntityOpenScene( BaseAction.BaseAction ):
 		if 'newSession' in arg:
 			newSession = arg['newSession']['value']
 
-		if extension == 'nk':
-			if pathExist:
-				if newSession:
-					Forge.core.Process.launchSoftware( 'c:/Program Files/Nuke8.0v3/Nuke8.0.exe', arg=path )
+		if pathExist or entity['version'] == 0:
+			if extension == 'nk':
+				if entity['version'] > 0:
+					if newSession:
+						Forge.core.Process.launchSoftware( 'c:/Program Files/Nuke8.0v3/Nuke8.0.exe', arg=path )
+					else:
+						import nuke
+						nuke.scriptOpen( path )
 				else:
-					import nuke
-					nuke.scriptOpen( path )
-			else:
-				if newSession:
-						Forge.core.Process().launchNukePython(
-						cmd='Hammer.Hnuke.Actions.CreateEntityInfo',
-						arg='entity=Hammer.getEntity(%i)' %( entity['entityId'] ),
-					)
-				else:
-					import Hammer.Hnuke.Actions
-					# todo : clean scene
-					Hammer.Hnuke.Actions.CreateEntityInfo( entity=entity )
+					if newSession:
+							Forge.core.Process().launchNukePython(
+							cmd='Hammer.Hnuke.Actions.CreateEntityInfo',
+							arg='entity=Hammer.getEntity(%i)' %( entity['entityId'] ),
+						)
+					else:
+						import Hammer.Hnuke.Actions
+						# todo : clean scene
+						Hammer.Hnuke.Actions.CreateEntityInfo( entity=entity )
 
-		elif extension == 'ma':
-			if pathExist:
-				if newSession:
-					Forge.core.Process.launchSoftware( 'c:/Program Files/Autodesk/Maya2015/bin/maya.exe', arg=path )
+			elif extension == 'ma':
+				if entity['version'] > 0:
+					if newSession:
+						Forge.core.Process.launchSoftware( 'c:/Program Files/Autodesk/Maya2015/bin/maya.exe', arg=path )
+					else:
+						import maya.cmds
+						maya.cmds.file( path, f=True, o=True )
 				else:
-					import maya.cmds
-					maya.cmds.file( path, f=True, o=True )
-			else:
-				if newSession:
-					Forge.core.Process().launchMayaPython(
-						cmd='Hammer.Hmaya.Actions.CreateEntityInfo',
-						arg='entity=Hammer.getEntity(%i)' %( entity['entityId'] ),
-					)
-				else:
-					import maya.cmds
-					import Hammer.Hmaya.Actions
-					maya.cmds.file( f=True, new=True )
-					self.createEntityInfo = Hammer.Hmaya.Actions.CreateEntityInfo( entity=entity )
+					if newSession:
+						Forge.core.Process().launchMayaPython(
+							cmd='Hammer.Hmaya.Actions.CreateEntityInfo',
+							arg='entity=Hammer.getEntity(%i)' %( entity['entityId'] ),
+						)
+					else:
+						import maya.cmds
+						import Hammer.Hmaya.Actions
+						maya.cmds.file( f=True, new=True )
+						self.createEntityInfo = Hammer.Hmaya.Actions.CreateEntityInfo( entity=entity )
+
+			else :
+				import Hammer.ui
+				self.popup = Hammer.ui.WindowInfo( title='Warning', info='The entity "%s" is not supported yet.' %(entity['entityType']) )
+				self.popup.show()
 
 		else :
 			import Hammer.ui
-			self.popup = Hammer.ui.WindowInfo( title='Warning', info='The entity "%s" is not supported yet.' %(entity['entityType']) )
-			self.popup.show()
+			self.popup = Hammer.ui.WindowInfo( title='Warning', info='No file found at this location : %s.' %(path) )
+ 			self.popup.show()
